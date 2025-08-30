7e1ac6e21cd1c8060de7aecde2af54b5fb6d86be
Mehul Lad <158362353+mehullad217@users.noreply.github.com>
2025-08-30 12:00:22 -0400

    Time: 19 ms (64.78%) | Memory: 17.7 MB (88.21%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.py
+++ 567-permutation-in-string/permutation-in-string.py
@@ -1,13 +1,23 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        from collections import Counter
-        h1=Counter(s1)
+        have = [0]*26
+        need=[0] *26
+        m,n = len(s1) , len(s2)
         l=0
-        for r in range(len(s2)):
-            r= l+len(s1)
-            h2 = Counter(s2[l:r])
-            if h1==h2:
+        r= 0
+
+        for i in s1:
+            need[ord(i)-ord('a')]+=1
+
+        while r<n:
+            have[ord(s2[r]) -ord('a')]+=1
+
+            if r-l+1> m:
+                have[ord(s2[l]) - ord('a')]-=1
+                l+=1
+            if r-l+1 ==m  and  have ==need:
                 return True
-            l+=1
+            r+=1
         
         return False
+        
