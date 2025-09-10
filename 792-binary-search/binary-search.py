280f865353aaa72c0d01746863b0fc5aa572b44f
Mehul Lad <158362353+mehullad217@users.noreply.github.com>
2025-09-10 10:35:19 -0400

    Time: 0 ms (100.00%) | Memory: 19 MB (6.11%) - LeetSync

--- 792-binary-search/binary-search.py
+++ 792-binary-search/binary-search.py
@@ -9,9 +9,7 @@ class Solution:
                 return middle
             elif nums[middle] < target:
                 l=middle+1
-            elif nums[middle] > target:
-                r=middle-1
             else:
-                return -1
+                r=middle-1
 
         return -1
