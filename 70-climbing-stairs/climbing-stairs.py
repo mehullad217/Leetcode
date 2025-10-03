b74577051467c639baa907b638bf22546fb25fc0
Mehul Lad <158362353+mehullad217@users.noreply.github.com>
2025-10-03 15:22:00 -0400

    Time: 0 ms (100.00%) | Memory: 17.7 MB (48.87%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.py
+++ 70-climbing-stairs/climbing-stairs.py
@@ -3,13 +3,14 @@ class Solution:
         
         if n==0:
             return 0
+
         if n==1:
             return 1
         
         dp = [0]*(n+1)
-
-        dp[1] =1
+        dp[1] = 1
         dp[2] =2
+
 
         for i in range(3, n+1):
             dp[i] = dp[i-2] +dp[i-1]
