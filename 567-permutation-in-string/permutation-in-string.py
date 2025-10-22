87c008eb9572e0746b2714fa80bad8beae8db111
Mehul Lad <158362353+mehullad217@users.noreply.github.com>
2025-10-22 10:37:50 -0400

    Time: 15 ms (84.33%) | Memory: 17.7 MB (87.88%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.py
+++ 567-permutation-in-string/permutation-in-string.py
@@ -1,23 +1,21 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
         have = [0]*26
-        need=[0] *26
-        m,n = len(s1) , len(s2)
+        need = [0]*26
         l=0
-        r= 0
-
         for i in s1:
-            need[ord(i)-ord('a')]+=1
+             have[(ord(i)- ord('a'))] +=1
 
-        while r<n:
-            have[ord(s2[r]) -ord('a')]+=1
-
-            if r-l+1> m:
-                have[ord(s2[l]) - ord('a')]-=1
+        for r in range(len(s2)):
+            need[(ord(s2[r])- ord('a'))] +=1
+            if r-l+1> len(s1):
+                need[(ord(s2[l])- ord('a'))] -=1
                 l+=1
-            if r-l+1 ==m  and  have ==need:
+            if have == need:
                 return True
-            r+=1
-        
-        return False
+
+
+        return False 
+
+
         
