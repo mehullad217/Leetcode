f893924ede4ac39cd0175cf8a5634d5958ed5d42
Mehul Lad <158362353+mehullad217@users.noreply.github.com>
2025-10-23 17:30:16 -0400

    Time: 0 ms (100.00%) | Memory: 17.6 MB (91.46%) - LeetSync

---
+++ 1081-video-stitching/video-stitching.py
@@ -0,0 +1,26 @@
+class Solution:
+    def videoStitching(self, clips: List[List[int]], time: int) -> int:
+        clips.sort()
+        n=len(clips)
+        max_end = 0
+        furthest_end = 0
+        used=0
+        i=0
+        while max_end<time:
+
+            while i<n and clips[i][0]<= max_end:
+                furthest_end = max(furthest_end , clips[i][1])
+
+                i+=1
+            if furthest_end == max_end:
+                return -1 
+            used+=1
+            max_end = furthest_end
+     
+
+        return used
+
+                
+                
+
+
